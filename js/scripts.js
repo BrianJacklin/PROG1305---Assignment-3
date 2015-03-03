0ad2712bdfcdc816e196a8a073c7f104aa515288
BJack <brianjacklinca@gmail.com>
2015-03-03 14:09:10 -0500

    updates based on bithound
    removed the function from the loop.

--- js/scripts.js
+++ js/scripts.js
@@ -32,67 +32,63 @@ window.onload = function() {
     while (preview.hasChildNodes()) {
       preview.removeChild(preview.lastChild);
     };
+    //this.classList.remove("active");
   }
 
-  for(var i = 0; i < allItems.length; i++){
+  function animate(e,i){
+    e.preventDefault(); // stop default click behaviour
+    hideAll(); // remove any child elements from the preview window
 
-    allItems[i].addEventListener("click", function(e){
+    var newNode = this.childNodes[0].firstChild.cloneNode(true); // newNode os a clone of the clicked image
+    preview.appendChild(newNode); // add newNode to the preview window
+    if(newNode.classList.contains("active") == 1){
+      newNode.classList.remove("active");
+    }else{
+      newNode.classList.add("active");
+    }
 
-      e.preventDefault(); // stop default click behaviour
-      hideAll(); // remove any child elements from the preview window
+    function Grow(){
 
-      var newNode = this.childNodes[0].firstChild.cloneNode(true); // newNode os a clone of the clicked image
-      preview.appendChild(newNode); // add newNode to the preview window
-      
-      if(newNode.classList.contains("active") == 1){
-          newNode.classList.remove("active");
+      var growStartTime = 0;
+      var growDuration = 600;
 
-      }else{
-              newNode.classList.add("active");
-
-      }
-
-      function Grow(){
-
-        var growStartTime = 0;
-        var growDuration = 600;
-
-        this.startGrow = function() {
-          growStartTime = Date.now();
-          requestAnimationFrame(update);
-        };
-
-        function update() {
-          var currentTime = Date.now();
-          var positionInGrow = (currentTime - growStartTime) / growDuration;
-
-          var h = positionInGrow * 100;
-          var w = positionInGrow * 100;
-
-          // Im not sure how we can make this better
-          // This is a hack to make sure that the height and width are both at 100
-          // when they are both 100% it fills its container without needing absolute values
-          // Is there a way to round
-          if (h > 100){
-            h = 100; 
-          }
-          if (w > 100){
-            w = 100;    
-          }
-            console.log('h: '+h + ' w: ' +w);
-          
-          newNode.style.height =  h + "%";
-          newNode.style.width =  w + "%";
-
-          if (positionInGrow <= 1)
-            requestAnimationFrame(update);
-         }
+      this.startGrow = function() {
+        growStartTime = Date.now();
+        requestAnimationFrame(update);
       };
+
+      function update() {
+        var currentTime = Date.now();
+        var positionInGrow = (currentTime - growStartTime) / growDuration;
+
+        var h = positionInGrow * 100;
+        var w = positionInGrow * 100;
+
+        // Im not sure how we can make this better
+        // This is a hack to make sure that the height and width are both at 100
+        // when they are both 100% it fills its container without needing absolute values
+        // Is there a way to round
+        if (h > 100){
+          h = 100; 
+        }
+        if (w > 100){
+          w = 100;    
+        }
+       
+        newNode.style.height =  h + "%";
+        newNode.style.width =  w + "%";
+
+        if (positionInGrow <= 1)
+          requestAnimationFrame(update);
+      }
+    };
+
       var gallery = new Grow();
       gallery.startGrow();
-      
-    },false); // allItems[i].onclick
+  } // animated
 
-  } // for i loop
+  for(var i = 0; i < allItems.length; i++){
+    allItems[i].addEventListener("click", animate); 
+  } 
 
 };// onload
